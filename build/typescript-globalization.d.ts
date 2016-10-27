92477110e0ca8a1e9b8d516dc53be997d4408276
Lukas Rögner <lukas.roegner@MacBook-Pro.fritz.box>
2016-10-27 18:10:38 +0200

    Removed the reference to jQuery.

--- build/typescript-globalization.d.ts
+++ build/typescript-globalization.d.ts
@@ -95,7 +95,6 @@ declare module 'Globalization/CultureInfo' {
 
 }
 
-/// <reference path="../../bower_components/DefinitelyTyped/jquery/jquery.d.ts" />
 /// <reference path="../../bower_components/DefinitelyTyped/moment/moment.d.ts" />
 /// <reference path="../../bower_components/DefinitelyTyped/numeraljs/numeraljs.d.ts" />
 declare module 'Globalization/DateTime' {
--- src/Typings/References.d.ts
+++ src/Typings/References.d.ts
@@ -1,4 +1,3 @@
 
-/// <reference path="../../bower_components/DefinitelyTyped/jquery/jquery.d.ts" />
 /// <reference path="../../bower_components/DefinitelyTyped/moment/moment.d.ts" />
 /// <reference path="../../bower_components/DefinitelyTyped/numeraljs/numeraljs.d.ts" />
